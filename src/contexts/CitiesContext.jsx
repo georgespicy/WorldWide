77a53835bf303abfdccd63c557c65fa7f81112c0
georgespicy <kolawolegeorge28743@gmail.com>
2024-03-29 10:16:55 +0100

    able to make the post request and improve the form user experincce

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -37,6 +37,26 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setCities((cities) => [...cities, data]);
+      setCurrentCity(data);
+    } catch {
+      alert("Failed to fetch cities");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -45,6 +65,7 @@ function CitiesProvider({ children }) {
         setCities,
         currentCity,
         getCity,
+        createCity,
       }}
     >
       {children}
